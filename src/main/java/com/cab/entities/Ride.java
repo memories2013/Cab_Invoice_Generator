e3f554c9897da66eac733588d8992594e6a14229
memories2013 <nshivaprasad41@gmail.com>
2021-12-23 20:29:55 +0530

    CREATE program to calculate cab fare

--- src/main/java/com/cab/entities/Ride.java
+++ src/main/java/com/cab/entities/Ride.java
@@ -2,4 +2,13 @@ package com.cab.entities;
 
 public class Ride {
 
+	public double distance;
+	public double time;
+	
+	public Ride(double distance, double time) {
+		super();
+		this.distance = distance;
+		this.time = time;
+	}
+	
 }
